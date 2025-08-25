061bf174728272406d925208ee5fc9bca6b03293
JakeChris2002 <blankenshipjc02@gmail.com>
2025-08-25 09:03:42 -0500

    Initial HelloWorld commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    private String name;
+
+    public HelloWorld(String name) {
+        this.name = name;
+    }
+
+    public void greet() {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld student = new HelloWorld("Jake");
+        student.greet();
+    }
+}
